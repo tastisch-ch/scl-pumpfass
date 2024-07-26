e5aac3d99970a82bf73b624fc69f1dfae3f9b83a
Chrigu Richard <christoph@tastisch.ch>
2024-07-26 22:27:22 +0200

    added function to toggle active language class

--- code/js/functions.js
+++ code/js/functions.js
@@ -22,3 +22,31 @@ document.querySelectorAll('.wrapper-language-switch [lang]').forEach((link) => {
         setTimeout(() => Weglot.switchTo(this.getAttribute('lang')), 160);
     });
 });
+
+// on Weglot init
+Weglot.on('initialized', () => {
+    // get the current active language
+    const currentLang = Weglot.getCurrentLang();
+    // if the current active language link exists
+    if ($('.wrapper-language-switch [lang=' + currentLang + ']')) {
+        // add is__active class
+        $('.wrapper-language-switch [lang=' + currentLang + ']').addClass("is-active-language");
+    }
+});
+
+// for each of the .wrapper__languages language links
+document.querySelectorAll('.wrapper-language-switch [lang]').forEach((link) => {
+    // add a click event listener
+    link.addEventListener('click', function(e) {
+        // prevent default
+        e.preventDefault();
+        // switch current active language after a setTimeout
+        setTimeout(() => Weglot.switchTo(this.getAttribute('lang')), 160);
+    });
+});
+
+
+$('.language-switch-btn').on('click', function() {
+    $('.language-switch-btn').removeClass('is-active-language');
+    $(this).toggleClass('is-active-language');
+});
