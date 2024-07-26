6b2bb494645a68dc4f6202466384868c92d1bfb8
Chrigu Richard <christoph@tastisch.ch>
2024-07-26 11:34:45 +0200

    added weglot functions

--- code/js/functions.js
+++ code/js/functions.js
@@ -1,1 +1,23 @@
 // Place for some code bratan!
+
+// on Weglot init
+Weglot.on('initialized', () => {
+    // get the current active language
+    const currentLang = Weglot.getCurrentLang();
+    // if the current active language link exists
+    if (document.querySelector('.wrapper-language-switch [lang=' + currentLang + ']')) {
+        // click the link
+        document.querySelector('.wrapper-language-switch [lang=' + currentLang + ']').click();
+    }
+});
+
+// for each of the .wg-element-wrapper language links
+document.querySelectorAll('.wrapper-language-switch [lang]').forEach((link) => {
+    // add a click event listener
+    link.addEventListener('click', function(e) {
+        // prevent default
+        e.preventDefault();
+        // switch current active language after a setTimeout
+        setTimeout(() => Weglot.switchTo(this.getAttribute('lang')), 160);
+    });
+});
