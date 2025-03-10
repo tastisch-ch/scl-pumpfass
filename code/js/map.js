56a462918625fc998949c47cf8655a54cef5925c
Chrigu Richard <christoph@tastisch.ch>
2025-03-10 15:21:28 +0100

    added map.js

---
+++ code/js/map.js
@@ -0,0 +1,231 @@
+function initMap() {
+    const styles = {
+      default: [
+        {
+            "featureType": "water",
+            "elementType": "geometry.fill",
+            "stylers": [
+                {
+                    "color": "#d3d3d3"
+                }
+            ]
+        },
+        {
+            "featureType": "transit",
+            "stylers": [
+                {
+                    "color": "#808080"
+                },
+                {
+                    "visibility": "off"
+                }
+            ]
+        },
+        {
+            "featureType": "road.highway",
+            "elementType": "geometry.stroke",
+            "stylers": [
+                {
+                    "visibility": "on"
+                },
+                {
+                    "color": "#b3b3b3"
+                }
+            ]
+        },
+        {
+            "featureType": "road.highway",
+            "elementType": "geometry.fill",
+            "stylers": [
+                {
+                    "color": "#ffffff"
+                }
+            ]
+        },
+        {
+            "featureType": "road.local",
+            "elementType": "geometry.fill",
+            "stylers": [
+                {
+                    "visibility": "on"
+                },
+                {
+                    "color": "#ffffff"
+                },
+                {
+                    "weight": 1.8
+                }
+            ]
+        },
+        {
+            "featureType": "road.local",
+            "elementType": "geometry.stroke",
+            "stylers": [
+                {
+                    "color": "#d7d7d7"
+                }
+            ]
+        },
+        {
+            "featureType": "poi",
+            "elementType": "geometry.fill",
+            "stylers": [
+                {
+                    "visibility": "on"
+                },
+                {
+                    "color": "#ebebeb"
+                }
+            ]
+        },
+        {
+            "featureType": "administrative",
+            "elementType": "geometry",
+            "stylers": [
+                {
+                    "color": "#a7a7a7"
+                }
+            ]
+        },
+        {
+            "featureType": "road.arterial",
+            "elementType": "geometry.fill",
+            "stylers": [
+                {
+                    "color": "#ffffff"
+                }
+            ]
+        },
+        {
+            "featureType": "road.arterial",
+            "elementType": "geometry.fill",
+            "stylers": [
+                {
+                    "color": "#ffffff"
+                }
+            ]
+        },
+        {
+            "featureType": "landscape",
+            "elementType": "geometry.fill",
+            "stylers": [
+                {
+                    "visibility": "on"
+                },
+                {
+                    "color": "#efefef"
+                }
+            ]
+        },
+        {
+            "featureType": "road",
+            "elementType": "labels.text.fill",
+            "stylers": [
+                {
+                    "color": "#696969"
+                }
+            ]
+        },
+        {
+            "featureType": "administrative",
+            "elementType": "labels.text.fill",
+            "stylers": [
+                {
+                    "visibility": "on"
+                },
+                {
+                    "color": "#737373"
+                }
+            ]
+        },
+        {
+            "featureType": "poi",
+            "elementType": "labels.icon",
+            "stylers": [
+                {
+                    "visibility": "off"
+                }
+            ]
+        },
+        {
+            "featureType": "poi",
+            "elementType": "labels",
+            "stylers": [
+                {
+                    "visibility": "off"
+                }
+            ]
+        },
+        {
+            "featureType": "road.arterial",
+            "elementType": "geometry.stroke",
+            "stylers": [
+                {
+                    "color": "#d6d6d6"
+                }
+            ]
+        },
+        {
+            "featureType": "road",
+            "elementType": "labels.icon",
+            "stylers": [
+                {
+                    "visibility": "off"
+                }
+            ]
+        },
+        {},
+        {
+            "featureType": "poi",
+            "elementType": "geometry.fill",
+            "stylers": [
+                {
+                    "color": "#dadada"
+                }
+            ]
+        }
+    ]
+  
+    };
+  
+    const map = new google.maps.Map(document.getElementById("map"), {
+        center: { lat: 47.161, lng: 8.440 },
+        zoom: 14,
+        mapTypeControl: false,
+        streetViewControl: false
+      });
+      
+      map.setOptions({ styles: styles['default'] });
+      
+      const locations = [
+        { lat: 46.96304, lng: 8.385601 },
+        { lat: 46.908607, lng: 8.252112 },
+        { lat: 46.859988, lng: 8.647938 }
+      ]; // Static locations
+      
+      const icon = {
+        path: google.maps.SymbolPath.CIRCLE,
+        scale: 10,
+        fillColor: "#024476",
+        fillOpacity: 1,
+        strokeWeight: 0
+      };
+      
+      locations.forEach(location => {
+        const marker = new google.maps.Marker({
+          position: location,
+          map: map,
+          icon: icon
+        });
+      
+        marker.addListener("click", () => {
+          const url = `https://www.google.com/maps/dir/?api=1&origin=Current+Location&destination=${location.lat},${location.lng}`;
+          window.open(url, '_blank');
+        });
+      });
+      }
+  
+  $(document).ready(function() {
+    initMap();
+  });
+  
